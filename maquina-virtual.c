aadac287ffc8b022d199c92ae13413637c5ce9f3
Valentina A.F. <valenvick23@gmail.com>
2025-09-01 23:52:32 -0300

    add data structures and file reading

--- maquina-virtual.c
+++ maquina-virtual.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+    int registros[32]; //32 registros de 32 bits
+    char memoria[16384];  //16 KiB
+    int tablasegmentos[8]; //8 entradas de 32 bits
+
+    FILE *archbin;
+    char aux;
+
+    archbin = fopen(argv[1], "rb");
+    if (archbin == NULL) {
+        printf("no se pudo abrir el archivo %s\n", argv[1]);
+    }
+    else {
+        fclose(archbin);
+    }
+    return 0;
+}
