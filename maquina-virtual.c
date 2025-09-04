c24955d78b66e17178bd889e6d506897e5fb3ae9
Valentina A.F. <valenvick23@gmail.com>
2025-09-03 22:44:06 -0300

    Update maquina-virtual.c

--- maquina-virtual.c
+++ maquina-virtual.c
@@ -1,45 +1,86 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #define TAM_MEMORIA 16384
 #define TAM_REGISTROS 32 
 #define TAM_TABLA_SEGMENTOS 8
+#define CANT_SEGMENTOS 2
+
+void terminarPrograma(char mensaje[]);
+
+void leerArchivoEntrada(char nombreArchivo[], char memoria[], int tablaSegmentos[], int registros[]);
+void inicializarTablaSegmentos(int tamanoCodigo, int tablaSegmentos[]);
+void inicializarPunterosInicioSegmentos(int tablaSegmentos[], int registros[]);
 
 int main(int argc, char *argv[]) {
     int registros[TAM_REGISTROS]; //32 registros de 32 bits
     char memoria[TAM_MEMORIA];  //16 KiB
     int tablasegmentos[TAM_TABLA_SEGMENTOS]; //8 entradas de 32 bits
 
-    leerArchivoEntrada(argv[1], memoria, tablasegmentos, registros);
-    ejecutarPrograma(memoria, registros, tablasegmentos);
+    leerArchivoEntrada("C:/Users/valen/OneDrive/Documentos/Facultad/Arquitectura/M-quina-virtual-v1/sample.vmx", memoria, tablasegmentos, registros);
+    //ejecutarPrograma(memoria, registros, tablasegmentos);
 
     return 0;
 }
 
-void leerArchivoEntrada(char nombreArchivo[], char memoria[], int segmentos[], int registros[]) {
-    FILE *archbin;
-    char aux; //variable auxiliar para leer byte a byte
+void leerArchivoEntrada(char nombreArchivo[], char memoria[], int tablaSegmentos[], int registros[]) {
+    FILE *archBin;
+    int16_t tamCodigo; //variable auxiliar para leer cada dos bytes
+    char lineaCodigo; //variable auxiliar para leer cada 1 byte
+    int i;
 
-    archbin = fopen(nombreArchivo, "rb");
-    if (archbin == NULL) {
-        printf("no se pudo abrir el archivo %s\n", nombreArchivo);
-    }
+
+    archBin = fopen(nombreArchivo, "rb");
+    if (archBin == NULL)
+        terminarPrograma("no se pudo abrir el archivo");
     else {
         //leer tamaño de codigo del archivo
-        //cargar codigo en memoria si es que no la excede
-        //inicializar tabla de segmentos
-        //inicializar punteros de inicio de segmentos
-        fclose(archbin);
+        fseek(archBin, 6, SEEK_SET);
+        if (fread(&tamCodigo, sizeof(tamCodigo), 1, archBin) != 1)
+            terminarPrograma("no se pudo leer el tamano del codigo");
+        else
+            if (tamCodigo > TAM_MEMORIA)
+                terminarPrograma("el tamamo del codigo supera al de la memoria");
+            else {
+                //cargar codigo en memoria
+                printf("%x", tamCodigo);
+                i = 0;
+                while (fread(&lineaCodigo, sizeof(lineaCodigo), 1, archBin) == 1) {
+                    memoria[i] = lineaCodigo;
+                    i++;
+                }
+                inicializarTablaSegmentos(tamCodigo, tablaSegmentos);
+                inicializarPunterosInicioSegmentos(tablaSegmentos, registros);
+            }
+        fclose(archBin);
     }
 }
 
 void inicializarTablaSegmentos(int tamanoCodigo, int tablaSegmentos[]) {
-    //inicializar tabla de segmentos
+    int aux = 0;
+    aux = aux | tamanoCodigo;
+    tablaSegmentos[0] = aux;
+    aux = aux << 16;
+    aux = aux | (TAM_MEMORIA - tamanoCodigo);
+    tablaSegmentos[1] = aux;
 }
 
 void inicializarPunterosInicioSegmentos(int tablaSegmentos[], int registros[]) {
-    //inicialiar DS, CS, IP
+    //inicialiar CS
+    registros[26] = 0;
+    //inicializar DS
+    registros[27] = 0;
+    registros[27] = registros[27] | 0x000010000;
+    //inicializar IP = CS
+    registros[3] = registros[26];
 }
 
 void ejecutarPrograma(char memoria[], int registros[], int tablaSegmentos[]) {
     //ciclo de fetch-decode-execute
+}
+
+void terminarPrograma(char mensaje[]) {
+    printf("%s\n", mensaje);
+    exit(EXIT_FAILURE);
 }
