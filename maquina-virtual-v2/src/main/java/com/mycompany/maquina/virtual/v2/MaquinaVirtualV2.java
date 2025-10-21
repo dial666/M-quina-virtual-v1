5e21125ad0d4492be1c4ef14874159a3ae1827bf
Valentina A.F. <valenvick23@gmail.com>
2025-10-20 21:59:49 -0300

    Proyecto creado

---
+++ maquina-virtual-v2/src/main/java/com/mycompany/maquina/virtual/v2/MaquinaVirtualV2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maquina.virtual.v2;
+
+/**
+ *
+ * @author valen
+ */
+public class MaquinaVirtualV2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
